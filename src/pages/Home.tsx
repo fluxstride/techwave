549c3bfd88d074035ee0127dadedfff39197b0d5
Samuel Adekoya <samueladexx@gmail.com>
2025-06-03 10:24:46 +0100

    update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router";
+
 const Home = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <h1 className="text-2xl sm:text-6xl text-center font-semibold">
-        Tech Wave'<span className=" text-[#0A80ED] font-bold">25</span>
-      </h1>
+      <div>
+        <h1 className="text-2xl sm:text-6xl text-center font-semibold">
+          Tech Wave'<span className=" text-[#0A80ED] font-bold">25</span>
+        </h1>
+
+        <Link
+          className="block bg-[#0A80ED] w-fit text-white p-2 px-4 rounded-md mx-auto mt-10"
+          to="/register"
+        >
+          Click to Register
+        </Link>
+      </div>
     </div>
   );
 };
